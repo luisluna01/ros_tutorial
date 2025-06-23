9043e34058d5dd44eaaf13966351a8f2b314b592
Luis Luna <luisluna12@utexas.edu>
2025-06-23 18:22:17 -0500

    Complete py publisher and subscriber tutorial

--- src/my_ros_tutorial_pkgs/my_ros_tutorial_cpp/cpp_pubsub/src/subscriber_member_function.cpp
+++ src/my_ros_tutorial_pkgs/my_ros_tutorial_cpp/cpp_pubsub/src/subscriber_member_function.cpp
@@ -24,7 +24,7 @@ class MinimalSubscriber : public rclcpp::Node
 {
 public:
   MinimalSubscriber()
-  : Node("minimal_subscriber")
+  : Node("minimal_subscriber") // give name to Node class constructor
   {
     subscription_ = this->create_subscription<std_msgs::msg::String>(
       "topic", 10, std::bind(&MinimalSubscriber::topic_callback, this, _1));

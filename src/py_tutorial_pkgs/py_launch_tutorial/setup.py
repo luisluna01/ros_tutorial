05a049fe32679d9f880eeba5d69acf76cbe78a21
Luis Luna <luisluna12@utexas.edu>
2026-06-09 22:30:11 -0500

    Ensure config directory is installed in py_launch_tutorial

--- src/py_tutorial_pkgs/py_launch_tutorial/setup.py
+++ src/py_tutorial_pkgs/py_launch_tutorial/setup.py
@@ -12,7 +12,9 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
-        (os.path.join('share', package_name, 'launch'), glob('launch/*'))
+        (os.path.join('share', package_name, 'launch'), glob('launch/*')),
+        (os.path.join('share', package_name, 'config'), glob('config/*.yaml')),
+        (os.path.join('share', package_name, 'rviz'), glob('config/*.rviz'))
     ],
     install_requires=['setuptools'],
     zip_safe=True,

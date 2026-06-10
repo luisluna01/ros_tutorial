f94401e301629a10d2370903e9890e25ec353dc1
Luis Luna <luisluna12@utexas.edu>
2026-06-09 20:21:37 -0500

    Complete Remapping from Launch: Managing Large Projects Tutorial

---
+++ src/py_tutorial_pkgs/py_launch_tutorial/launch/mimic.launch.py
@@ -0,0 +1,17 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+
+def generate_launch_description():
+    return LaunchDescription([
+        Node(
+            package='turtlesim',
+            executable='mimic',
+            name='mimic',
+            remappings=[
+                # Remap first topics in first element to topics in second element
+                ('/input/pose', '/turtle2/pose'),
+                ('/output/cmd_vel', '/turtlesim2/turtle1/cmd_vel')
+            ]
+        )
+    ])

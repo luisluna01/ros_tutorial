eaaf24567e90ae4df5ccf3d959f520d28a359d2d
Luis Luna <luisluna12@utexas.edu>
2026-06-09 22:17:23 -0500

    Complete config files section - From Launch Tutorial: Managing Large Projects

---
+++ src/py_tutorial_pkgs/py_launch_tutorial/launch/turtlesim_rviz.launch.py
@@ -0,0 +1,17 @@
+from launch import LaunchDescription
+from launch.substitutions import PathJoinSubstitution
+from launch_ros.actions import Node
+from launch_ros.substitutions import FindPackageShare
+
+
+# Start RViz with config file from 'turtle_tf2_py' package
+def generate_launch_description():
+    return LaunchDescription([
+        Node(
+            package='rviz2',
+            executable='rviz2',
+            name='rviz2',
+            arguments=['-d', PathJoinSubstitution([
+                FindPackageShare('turtle_tf2_py'), 'rviz', 'turtle_rviz.rviz'])],
+        ),
+    ])

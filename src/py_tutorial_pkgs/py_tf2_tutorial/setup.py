3d1053c689f6332bbe2af284e8d7d93fef05648e
Luis Luna <luisluna12@utexas.edu>
2026-06-10 23:08:53 -0500

    Create python pkg for tf2 tutorial

---
+++ src/py_tutorial_pkgs/py_tf2_tutorial/setup.py
@@ -0,0 +1,29 @@
+from setuptools import find_packages, setup
+
+package_name = 'py_tf2_tutorial'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='Luis Luna',
+    maintainer_email='luisluna12@utexas.edu',
+    description='Python pkg for learning tf2 in ROS2',
+    license='Apache-2.0',
+    extras_require={
+        'test': [
+            'pytest',
+        ],
+    },
+    entry_points={
+        'console_scripts': [
+        ],
+    },
+)

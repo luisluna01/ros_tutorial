0dad5b9d48f1a2479367e57d516bbda19f779a47
Luis Luna <luisluna12@utexas.edu>
2026-06-15 13:09:55 -0500

    Modify turtle_tf2_demo launch file in py_tf2_tutorial - Add broadcaster and listener

--- src/py_tutorial_pkgs/py_tf2_tutorial/launch/turtle_tf2_demo.launch.py
+++ src/py_tutorial_pkgs/py_tf2_tutorial/launch/turtle_tf2_demo.launch.py
@@ -1,9 +1,16 @@
 from launch import LaunchDescription
+
+from launch.actions import DeclareLaunchArgument
+from launch.substitution import LaunchConfiguration
 from launch_ros.actions import Node
 
 
 def generate_launch_description():
     return LaunchDescription([
+        DeclareLaunchArgument(
+            'target_frame', default_value='turtle1',
+            description='Target frame name'
+        ),
         Node(
             package='turtlesim',
             executable='turtlesim_node',
@@ -17,4 +24,20 @@ def generate_launch_description():
                 {'turtlename': 'turtle1'}
             ]
         ),
+        Node(
+            pacakge='py_tf2_tutorial',
+            executable='turtle_tf2_broadcaster',
+            name='broadcaster2',
+            parameters=[
+                {'turtlename': 'turtle2'}
+            ]
+        ),
+        Node(
+            pacakge='py_tf2_tutorial',
+            executable='turtl2_tf2_listener',
+            name='listener',
+            parameters=[
+                {'target_frame': LaunchConfiguration('target_frame')}
+            ]
+        )
     ])
